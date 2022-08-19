88a2f0597cd5d7e084fda651f70a8f91da7e73e9
Your Name <you@example.com>
2022-08-19 11:32:19 +0700

    first commit

--- js/script.js
+++ js/script.js
@@ -10,11 +10,57 @@ $(document).on("scroll", function(){
 		}
 });
 
+$(document).ready(function(){       
+  var scroll_pos = 0;
+  $(document).scroll(function() { 
+      scroll_pos = $(this).scrollTop();
+      if(scroll_pos > 86) {
+          $('.nav-link').css('color', '#000');
+          // $('#banner').css('color', '#000');
+      } else {
+          $('.nav-link').css('color', '#000');
+          // $('#banner').css('color', '#000');
+
+      }
+  });
+});
+
+$(window).scroll(function() {
+  var image = document.getElementById("navbarImg");
+  if (document.documentElement.scrollTop > 86)
+      image.src =
+      "/assets/condfe-purple.png";
+  else
+      image.src =
+      "/assets/condfe-purple.png";
+})
+
+var swiper = new Swiper(".swiperFull", {
+  // Optional parameters
+  slidesPerView: 1,
+  loop: true,
+
+   autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+   },
+   allowTouchMove: true,
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
+
+})
+
 
 var swiper = new Swiper(".mySwiper", {
         spaceBetween: 30,
         centeredSlides: true,
         loop: true,
+        autoplay: {
+          delay: 1200,
+        },
         pagination: {
           el: ".swiper-pagination",
           clickable: true,
@@ -22,8 +68,8 @@ var swiper = new Swiper(".mySwiper", {
         breakpoints: {
             // when window width is >= 320px
             320: {
-              slidesPerView: 2,
-              spaceBetween: 20
+              slidesPerView: 2.3,
+              spaceBetween: 10
             },
             // when window width is >= 480px
             480: {
@@ -36,8 +82,50 @@ var swiper = new Swiper(".mySwiper", {
               spaceBetween: 40
             },
             1200: {
-              slidesPerView: 7,
+              slidesPerView: 8,
               spaceBetween: 40
             }
-          }
+        }
 });
+
+
+// Modal Function
+// Modal Function //
+const scriptURL =
+  'https://script.google.com/macros/s/AKfycby1RbCmM4mcFsdqsFeSSqwWY0jHmqAOO-WZk_TMuaB89zfmdj8KhpdgucQZtW5BVFQXQA/exec'
+const form = document.forms['condfe-form'];
+const btnKirim = document.querySelector('.btn-kirim');
+const btnLoading = document.querySelector('.btn-loading');
+const myAlert = document.querySelector('.my-alert');
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+
+  // ketika tombol submit di click 
+  // tampilkan tombol loading hilangkan tombol kirim 
+  btnLoading.classList.toggle('d-none');
+  btnKirim.classList.toggle('d-none');
+
+  fetch(scriptURL, {
+      method: 'POST',
+      body: new FormData(form)
+    })
+    // tampilkan tombol kirim hilangkan tombol kirim 
+
+    .then((response) => {
+      btnLoading.classList.toggle('d-none');
+      btnKirim.classList.toggle('d-none');
+      // tampilkan alert 
+      myAlert.classList.toggle('d-none')
+      // reset form
+      form.reset();
+      console.log('Success!', response)
+    })
+
+    .catch(error => console.error('Error!', error.message))
+})
+
+// autoplay video
+window.onload = function() {
+  document.getElementById("autoplay").play();
+}
